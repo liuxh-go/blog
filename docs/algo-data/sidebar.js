636bb7ebe04c0a5a31434c393051b06c96a94f65
lxh <liuxh@wanthings.com>
2019-12-11 16:46:44 +0800

    修复bug

--- docs/algo-data/sidebar.js
+++ docs/algo-data/sidebar.js
@@ -1,9 +1,9 @@
 module.exports = [
     {
         title: '数据结构与算法',
-        children: [
-             require('./algo/sidebar'),
-             require('./data/sidebar')
-        ]
+        children: {
+             '算法': require("./algo/sidebar"),
+             '数据结构': require("./data/sidebar")
+        }
     }
 ]
